e310936359a9205518905af728834614d4436925
Marek Cech <cechsmarek@gmail.com>
2024-07-01 19:17:04 +0200

    Added database and predict.html rendering

--- app.py
+++ app.py
@@ -3,14 +3,34 @@ from pytube import YouTube
 from pydub import AudioSegment
 import librosa
 from transformers import pipeline
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+# creating db model
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+with app.app_context():
+    print("test")
+    db.create_all()
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    youtube_link = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f'<Song {self.name}>'
 
 def download_youtube_video(url):
     yt = YouTube(url,use_oauth=True, allow_oauth_cache=True)
+    title = yt.title  # Get the video title
     stream = yt.streams.filter(only_audio=True).first()
     out_file = stream.download(output_path="download/")
-    return out_file
+    return out_file, title
 
 def convert_to_wav(mp4_file):
     audio = AudioSegment.from_file(mp4_file, format="mp4")
@@ -23,7 +43,7 @@ def prediction(wav_file):
     data, sr = librosa.load(wav_file)
     data = librosa.resample(data, orig_sr=sr, target_sr=target_sr)
 
-    classifier = pipeline("audio-classification", model="MarekCech/GenreVim-HuBERT-1")
+    classifier = pipeline("audio-classification", model="MarekCech/GenreVim-HuBERT-3")
     return classifier(data)
 
 """
@@ -41,8 +61,16 @@ def index():
 @app.route("/predict", methods=['POST'])
 def predict():
     url = request.form['url']
-    file = download_youtube_video(url)
+    file, title = download_youtube_video(url)
     wav_file = convert_to_wav(file)
-    predict = prediction(wav_file)
-    predict = predict[0]['label']
-    return predict
+    genre = prediction(wav_file)
+    genre = genre[0]['label']
+    
+    new_song = Song(name=title, genre=genre, youtube_link=url)
+    db.session.add(new_song)
+    db.session.commit()
+    
+    recommendations = Song.query.filter_by(genre=genre).all()
+    return render_template('predict.html', genre=f"Genre of '{title}' is '{genre}'", recommendations=recommendations)
+
+
