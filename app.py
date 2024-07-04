3ab67eb5918a6d473f67ac44081793afea3641a3
Marek Cech <cechsmarek@gmail.com>
2024-07-04 16:36:30 +0200

    added music detection module and music detection notebook

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 from pytube import YouTube
 from pydub import AudioSegment
 import librosa
 from transformers import pipeline
 from flask_sqlalchemy import SQLAlchemy
-
+from bs4 import BeautifulSoup
+import requests
+import os
 
 app = Flask(__name__)
 # creating db model
@@ -27,10 +29,19 @@ class Song(db.Model):
 
 def download_youtube_video(url):
     yt = YouTube(url,use_oauth=True, allow_oauth_cache=True)
-    title = yt.title  # Get the video title
+    if yt.length > 7*60:  # Set a maximum video length
+        raise ValueError("Video is too long. Maximum length is 7 minutes.")
+    if yt.length < 30:  # Set a minimum video length 
+        raise ValueError("Video is too short. Minimum length is 30 seconds.")
     stream = yt.streams.filter(only_audio=True).first()
     out_file = stream.download(output_path="download/")
-    return out_file, title
+    return out_file
+
+def get_video_title(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find('meta', property='og:title')
+    return title['content'] if title else 'Unknown Title'
 
 def convert_to_wav(mp4_file):
     audio = AudioSegment.from_file(mp4_file, format="mp4")
@@ -42,9 +53,14 @@ def prediction(wav_file):
     target_sr = 16000
     data, sr = librosa.load(wav_file)
     data = librosa.resample(data, orig_sr=sr, target_sr=target_sr)
-
-    classifier = pipeline("audio-classification", model="MarekCech/GenreVim-HuBERT-3")
-    return classifier(data)
+    
+    music_classifier = pipeline("audio-classification", model="MarekCech/GenreVim-Music-Detection-DistilHuBERT")
+    music_prediction = music_classifier(data, sampling_rate=target_sr)
+    if music_prediction[0]['label'] == 'Non Music':
+        return None, "The provided video does not contain music. Please try another video."
+    genre_classifier = pipeline("audio-classification", model="MarekCech/GenreVim-HuBERT-3")
+    genre_prediction = genre_classifier(data, sampling_rate=target_sr)
+    return genre_prediction[0]['label'], None
 
 """
 @app.route("/",methods=['GET', 'POST'])
@@ -61,10 +77,17 @@ def index():
 @app.route("/predict", methods=['POST'])
 def predict():
     url = request.form['url']
-    file, title = download_youtube_video(url)
+    file = download_youtube_video(url)
+    title = get_video_title(url)
     wav_file = convert_to_wav(file)
-    genre = prediction(wav_file)
-    genre = genre[0]['label']
+    genre, error_message = prediction(wav_file)
+        
+    if error_message:
+        os.remove(file)
+        os.remove(wav_file)
+        return render_template('predict.html', genre=error_message)
+    
+    #genre = genre[0]['label']
     
     new_song = Song(name=title, genre=genre, youtube_link=url)
     db.session.add(new_song)
